0b4d8294fb737c57ea5b3303d155ee640d07b3f6
Russell Creswell <rucreswe@cisco.com>
2021-07-13 09:45:19 -0400

    Added models

--- xword_data/models.py
+++ xword_data/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Puzzle(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    date = models.DateTimeField('date published', blank=False, null=False)
+    byline = models.CharField(max_length=255, blank=False)
+    publisher = models.CharField(max_length=12, blank=False)
+
+class Entry(models.Model):
+    entry_text = models.CharField(max_length=50, blank=False, unique=True)
+
+class Clue(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    clue_text = models.CharField(max_length=512, blank=False)
+    theme = models.BooleanField(default=False)
